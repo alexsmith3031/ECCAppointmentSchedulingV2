86bf6d5830140bd0f49f87de9f9cb5abc114cb0e
rafaelgonzales <rga281@gmail.com>
2017-11-10 14:46:47 -0600

    commiting customer with validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,13 @@ class Customer < ApplicationRecord
   accepts_nested_attributes_for :appointment
   accepts_nested_attributes_for :vehicle
 
+  validates :email, :firstName, :lastName, presence: true
 
+  validates :phone, :presence => true,
+            length: {minimum: 10, maximum: 15,},
+            format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/,
+            message: ('number is not valid')}
+
+  validates_format_of :email, :with => /\A[^@,\s]+@[^@,\s]+\.[^@,\s]+\z/
+    #validates :email, format: /@/
 end
