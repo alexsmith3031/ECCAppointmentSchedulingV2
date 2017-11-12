2b08ecf6bdd66b306ec0d579053950c76fdb4bd5
Alex Smith <alexsmith3031@gmail.com>
2017-11-11 19:51:38 -0600

    merged login branch, styled css, customer index is now employee facing side, linked data source with postgres for system. may need to remove

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  #validates :employee_name, :presence => true
+
+  #validates_format_of :employee_name, :with => /\A[a-z]+\z/i,
+                      #message: (' cannot include numeric digits or special characters'),
+                      #:allow_blank => true
+
 end
